14337a4a88e97ea2078fb9746c80a49940e667a9
Shelby Rothman <shelby.rothman@aderant.com>
2023-08-04 11:38:55 -0700

    error handeling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import './App.css';
 import { useAuthContext } from './AuthProvider';
-
+// import { useP}
 
 
 function App() {
     const { session, displayName, setNewPassword, closeIt, setCloseIt } = useAuthContext();
     const inputRefPassword = useRef<HTMLInputElement>(null);
     const inputRefConfirmPassword = useRef<HTMLInputElement>(null);
+    const [ error, setError ] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (window.location.href.includes('error')) setError(window.location.href.split('error=')[ 1 ]);
+    }, [])
 
     function handleSubmit(event: any) {
         event.preventDefault();
@@ -24,26 +29,32 @@ function App() {
 
     return (
         <>
-            {closeIt ? (<div>
-                <h2>Update successful. You can now close this window.</h2>
-                <h2>Return to your GRAPES app to login with your new credentials!</h2>
-            </div>) : (<div>
-                <h1>GRAPES Account Updates</h1>
-                {session == null ? <div>Loading...</div> : (<>
-                    <h3>Hi {displayName}, let's reset your password.</h3>
-                    <div>
-                        <label>New Password:</label>{' '}
-                        <input ref={inputRefPassword} type="password" id="newPassword" minLength={6} required />
-                        <br />
-                        <br />
-                        <label>Confirm New Password:</label>{' '}
-                        <input ref={inputRefConfirmPassword} type="password" id="confirmNewPassword" required />
-                        <br />
-                        <br />
-                        <button type="button" onClick={handleSubmit}>submit</button>
-                    </div>
-                </>)}
-            </div>)}
+            {error ? <div>
+                <h2>There was an error updating your account. Please try again later.</h2>
+                <h4><i>{error}</i></h4>
+                <p>Go Get a new link.</p>
+            </div> : (<>
+                {closeIt ? (<div>
+                    <h2>Update successful. You can now close this window.</h2>
+                    <h2>Return to your GRAPES app to login with your new credentials!</h2>
+                </div>) : (<div>
+                    <h1>GRAPES Account Updates</h1>
+                    {session == null ? <div>Loading...</div> : (<>
+                        <h3>Hi {displayName}, let's reset your password.</h3>
+                        <div>
+                            <label>New Password:</label>{' '}
+                            <input ref={inputRefPassword} type="password" id="newPassword" minLength={6} required />
+                            <br />
+                            <br />
+                            <label>Confirm New Password:</label>{' '}
+                            <input ref={inputRefConfirmPassword} type="password" id="confirmNewPassword" required />
+                            <br />
+                            <br />
+                            <button type="button" onClick={handleSubmit}>submit</button>
+                        </div>
+                    </>)}
+                </div>)}
+            </>)}
         </>
     )
 }
